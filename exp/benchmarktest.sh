4919f75dfd0a85011403b6c714007ae707a8ae3a
Dixing (Dex) Xu <dixingxu@gmail.com>
2020-12-25 15:25:28 +0800

    update test script

--- exp/benchmarktest.sh
+++ exp/benchmarktest.sh
@@ -3,7 +3,8 @@ mpirun --prefix <location of openmpi> -np <number of processes> -hostfile <dns o
 # example
 
 # ring algorithm
-# message size up to 512MB
+# message size up to 1GB
+# memroy limit up to 8GB (7GB available)
 # force using eth0 interface
 # debug mode
-mpirun --prefix <location of openmpi> -np <number of processes> -hostfile <dns of servers> --mca btl_tcp_if_include 192.168.0.1/24 --mca orte_base_help_aggregate 0 --mca coll_tuned_allreduce_algorithm 4 <location to omb>/mpi/collective/osu_allreduce -m 536870912
+mpirun --prefix <location of openmpi> -np <number of processes> -hostfile <dns of servers> --mca btl_tcp_if_include 192.168.0.1/24 --mca orte_base_help_aggregate 0 --mca coll_tuned_allreduce_algorithm 4 <location to omb>/mpi/collective/osu_allreduce -m 1073741824 -M 7550136320
